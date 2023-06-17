459fa924d5b1fc4ff6c613a766354248ef6625cd
K14KHaNaSa <chjone0417@gmail.com>
2023-06-18 02:44:19 +0900

    230618 0244

---
+++ 27889.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main(){
+    var sc : Scanner = Scanner(System.`in`)
+    var sch = sc.nextLine()
+    if(sch=="NLCS")
+        println("North London Collegiate School")
+    if(sch=="BHA")
+        println("Branksome Hall Asia")
+    if(sch=="KIS")
+        println("Korea International School")
+    if(sch=="SJA")
+        println("St. Johnsbury Academy")
+}
